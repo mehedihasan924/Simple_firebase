dbb4e5987987795d630f0b9a916515c90eba05bc
Mehedi-Hasan <mdhasanmehedi924@gmail.com>
2023-10-30 13:35:22 -0700

    Update User Information

--- src/Component/LoginFrom/LoginFrom.jsx
+++ src/Component/LoginFrom/LoginFrom.jsx
@@ -1,5 +1,5 @@
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
-import React, { useState } from 'react';
+import { getAuth, sendPasswordResetEmail, signInWithEmailAndPassword } from 'firebase/auth';
+import React, { useRef, useState } from 'react';
 import app from '../Firebase2/firebase.config';
 import { Link } from 'react-router-dom';
 const auth = getAuth(app);
@@ -7,13 +7,14 @@ const auth = getAuth(app);
 const LoginFrom = () => {
         const [error, setError]=useState('');
         const [success, setSuccess]=useState('')
+        const  emailRef=useRef();
 
     const handleLogin=(event)=>{
         event.preventDefault();
           const from=event.target;
           const email=from.email.value;
           const password=from.password.value
-          console.log(email, password);
+          // console.log(email, password);
 
    // validation
        setError('')
@@ -43,29 +44,48 @@ const LoginFrom = () => {
            .then(result=> {
              const loginUser=result.user;
              setSuccess("User login successfily.");         
-             setError('');
+             setError(loginUser);
           
            })
            .catch(error=>{
              setError(error.massage)
            })
     }
+
+ const handleRestPassword=(event)=>{
+   const email=emailRef.current.value;
+    if(!email){
+      alert("Please provide your Email Address to reset password")
+      return;
+    }
+    sendPasswordResetEmail(auth ,email)
+    .then(()=>{
+      alert("Please Check your email")
+    })
+    .catch(error=>{
+        console.log(error);
+        setError(error.massage)
+    })
+ }
+
+
+
     return (
         <div className='w-50 mx-auto'>
         <h1> Login From </h1>
        <form  onSubmit={handleLogin}>
-           <input required className='w-50 mb-2 rounded ps-2'  type="email" name="email" id="email" placeholder='Your Email' />
+          
+           <input required className='w-50 mb-2 rounded ps-2' ref={emailRef}  type="email" name="email" id="email" placeholder='Your Email' />
            <br />
            <input  required className='w-50 mb-2 rounded ps-2'  type="password" name="password" id='password' placeholder='Your Password' />
            <br />
            <h6 className='text-danger'>  </h6>
            <input className='btn btn-primary' style={{marginTop:"10px"}} type="submit" value="Login" />
         <p> <small> Are you new user? <Link to="/register"> Register Now</Link> </small></p>
-
-       <h3>{error}</h3>
-       <h3>{success}</h3>
      </form>
-      
+        <p>Forget Password Please <button onClick={handleRestPassword} className='px-3' > Reset</button></p>
+     <h3>{error}</h3>
+       <h3>{success}</h3>
     </div>
     );
 };
--- src/Component/Register/Register.jsx
+++ src/Component/Register/Register.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, sendEmailVerification, updateProfile } from "firebase/auth";
 // Fireabase connent
 import app from '../Firebase2/firebase.config';
 const auth = getAuth(app);
@@ -11,7 +11,7 @@ const Register = () => {
    // Error massage show korar jonno
    const [error, setError]=useState('')
     const [email, setemail]=useState('')
-
+    const [success, setSuccess]=useState('')
 // Input field data show system 
     const handleEmailChange=(event)=>{
         console.log(event.target.value);
@@ -27,8 +27,11 @@ const Register = () => {
        //2. Collect from data
         const email=event.target.email.value;
         const password=event.target.password.value
-        console.log(email, password);
+        const name= event.target.password.value
+
+        console.log(email, password, name);
               // Validation
+
                 if(!/(?=.*[A-Z])/.test(password)){
                     setError('Please Add 2 Uparcase');
                     return;
@@ -40,23 +43,57 @@ const Register = () => {
                 else if(password.length<6){
                     setError('Please Atlest 6 Chaacter input')
                 }
+                else{
+                    setSuccess("You Successfuly login");
+                    event.target.reset()
+                   }
                 
             //  3. Creat user ib firebase
-            createUserWithEmailAndPassword(auth ,email, password )
+            createUserWithEmailAndPassword(auth ,email, password)
                 .then(result=>{
                     const loginUser=result.user;
                     console.log(loginUser);
-                    setError(" Success Full login") 
+                    setError(" ") ;
+                //Email verifay    
+                    if(!loginUser.emailverified){
+                        setError(" Please Check  Your Email Verification ")   
+                    }
+
                  // Sob Clear click on submit button   
                     event.target.reset();
-                })
-                
-                .catch(error=>{
-                    console.error(error.massage);
-                    setError( " Password minmum 6 caterter ")
-                })
-    }
 
+
+                    //Email verifay   
+                    sendVerificationEmail(result.user);
+                     // User Info name
+                     updateUserData(result.user, name )
+                })           
+                .catch(error=>{
+                    setError(error.massage)
+                  })
+            }     
+
+         //    Verificaton Email
+         const sendVerificationEmail=(user)=>{
+                sendEmailVerification(user)
+                .then(result=>{
+                    console.log(result);
+                    alert("Please verify your email address")
+                })
+
+         }
+
+         const updateUserData=(user, name)=>{
+                updateProfile(user,{
+                    displayName:name
+                })
+                .then(()=>{
+                    console.log("User name upded");
+                }) 
+                .catch(error =>{
+                    setError(error.massage)
+                })
+         }
 
 // Blur Password Show to console
     const handlePasswordBlur=(event)=>{
@@ -72,11 +109,14 @@ const Register = () => {
             <h1>{email}</h1>
 
            <form  onSubmit={handleSubmit}>
+             <input required className='w-50 mb-2 rounded ps-2' onChange={handleEmailChange} type="name" name="name" id="ame" placeholder='Your Name' />
+               <br />
                <input required className='w-50 mb-2 rounded ps-2' onChange={handleEmailChange} type="email" name="email" id="email" placeholder='Your Email' />
                <br />
                <input  required className='w-50 mb-2 rounded ps-2' onBlur={handlePasswordBlur} type="password" name="password" id='password' placeholder='Your Password' />
                <br />
                <h6 className='text-danger'> {error} </h6>
+               <h1> {success}</h1>
                <input className='btn btn-primary' style={{marginTop:"10px"}} type="submit" value="Register" />
                <p> <small> Are you all ready loged ? <Link to="/loginfrom"> Login</Link> </small></p>
          </form>
